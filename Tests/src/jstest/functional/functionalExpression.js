b21c22854fd966d748b382efcb11c83fe9df9062
ricnorr <31213770+ricnorr@users.noreply.github.com>
2020-03-19 00:08:47 +0300

    fdkfds

--- Tests/src/jstest/functional/functionalExpression.js
+++ Tests/src/jstest/functional/functionalExpression.js
@@ -1,35 +1,76 @@
 "use strict";
 
+let varNames = {"x" : 0, "y" : 1, "z" : 2};
+let constNames = {"e" : Math.E, "pi" : Math.PI};
+
 function binOperation(a, b, func) {
-    function evaluate(x) {
-        return func(a(x), b(x));
+    function evaluate() {
+        return func(a.apply(this, arguments), b.apply(this, arguments));
     }
     return evaluate;
 }
 
 function negate(x) {
-    function evaluate(x) {
+    function evaluate() {
         return -x;
     }
     return evaluate;
 }
 
+function cube(x) {
+    function evaluate() {
+        return Math.pow(x.apply(this, arguments), 3);
+    }
+    return evaluate;
+}
+
+function cuberoot(x) {
+    function evaluate() {
+        return Math.pow(x.apply(this, arguments), 1/3);
+    }
+    return evaluate;
+}
+
 function variable(a) {
-    function evaluate(x) {
-        return x;
+    let name = a;
+    function evaluate() {
+        for (let i = 0; i < arguments.length; i++)
+        {
+            if (varNames[name] == i) {
+                return arguments[i];
+            }
+        }
     }
     return evaluate;
 }
 
 function cnst(a) {
-    function evaluate(x) {
-        return a;
+    function evaluate() {
+        if (a in constNames) {
+            return constNames[a];
+        } else {
+            return a;
+        }
     }
     return evaluate;
 }
 
 function add(a, b) {
     return binOperation (a, b, (x, y) => x + y);
+}
+
+function sin(a) {
+    function evaluate() {
+        return Math.sin(a.apply(this, arguments));
+    }
+    return evaluate;
+}
+
+function cos(a) {
+    function evaluate() {
+        return Math.cos(a.apply(this, arguments));
+    }
+    return evaluate;
 }
 
 function subtract(a, b) {
@@ -85,8 +126,22 @@ function parse(a) {
                 t = stack.pop();
                 stack.push(subtract(stack.pop(), t));
                 break;
+            case("sin"):
+                stack.push(sin(stack.pop()));
+                break;
+            case("cos"):
+                stack.push(cos(stack.pop()));
+                break;
+            case("cuberoot"):
+                stack.push(cuberoot(stack.pop()));
+                break;
+            case("cube"):
+                stack.push(cube(stack.pop()));
+                break;
             default:
-                if (!isNaN(token[i])) {
+                if (token[i] in constNames) {
+                    stack.push(cnst(token[i]));
+                } else if (!isNaN(token[i])) {
                     stack.push(cnst(parseInt(token[i])));
                 } else {
                     stack.push(variable(token[i]));
@@ -94,4 +149,9 @@ function parse(a) {
         }
     }
     return stack[0];
+}
+
+let exp = parse("x y +");
+for (let i = 0; i++; i < 10) {
+    console.log(exp(i));
 }
--- task6/.idea/jstest/functional/FunctionalExpression.js
+++ task6/.idea/jstest/functional/FunctionalExpression.js
@@ -1,35 +1,76 @@
 "use strict";
 
+let varNames = {"x" : 0, "y" : 1, "z" : 2};
+let constNames = {"e" : Math.E, "pi" : Math.PI};
+
 function binOperation(a, b, func) {
-    function evaluate(x) {
-        return func(a(x), b(x));
+    function evaluate() {
+        return func(a.apply(this, arguments), b.apply(this, arguments));
     }
     return evaluate;
 }
 
 function negate(x) {
-    function evaluate(x) {
+    function evaluate() {
         return -x;
     }
     return evaluate;
 }
 
+function cube(x) {
+    function evaluate() {
+        return Math.pow(x.apply(this, arguments), 3);
+    }
+    return evaluate;
+}
+
+function cuberoot(x) {
+    function evaluate() {
+        return Math.pow(x.apply(this, arguments), 1/3);
+    }
+    return evaluate;
+}
+
 function variable(a) {
-    function evaluate(x) {
-        return x;
+    let name = a;
+    function evaluate() {
+        for (let i = 0; i < arguments.length; i++)
+        {
+            if (varNames[name] == i) {
+                return arguments[i];
+            }
+        }
     }
     return evaluate;
 }
 
 function cnst(a) {
-    function evaluate(x) {
-        return a;
+    function evaluate() {
+        if (a in constNames) {
+            return constNames[a];
+        } else {
+            return a;
+        }
     }
     return evaluate;
 }
 
 function add(a, b) {
     return binOperation (a, b, (x, y) => x + y);
+}
+
+function sin(a) {
+    function evaluate() {
+        return Math.sin(a.apply(this, arguments));
+    }
+    return evaluate;
+}
+
+function cos(a) {
+    function evaluate() {
+        return Math.cos(a.apply(this, arguments));
+    }
+    return evaluate;
 }
 
 function subtract(a, b) {
@@ -44,35 +85,23 @@ function divide(a, b) {
     return binOperation(a, b, (x,y) => x / y);
 }
 
-function skipWhitespace(s, i) {
-    while (a[i] == " ") {
+function skipToken(s, i, beSkiped) {
+    while (beSkiped(s[i]) && i < s.length) {
         i++;
     }
     return i;
 }
 
-function reverse(a, b) {
-    return b,a;
-}
-
 function split(s) {
     let i = 0;
     let tokens = [];
-    while (s[i] == " ") {
-        i++;
-    }
+    i = skipToken(s, i, (s) => (s == " "));
     while (i < s.length) {
-        while (s[i] == " ") {
-            i++;
-        }
+        i = skipToken(s, i, (s) => (s == " "));
         let l = i;
-        while (s[i] != " " && i < s.length) {
-            i++;
-        }
+        i = skipToken(s, i, (s) => (s != " "));
         tokens.push(s.substring(l, i));
-        while (s[i] == " ") {
-            i++;
-        }
+        i = skipToken(s, i, (s) => (s == " "));
     }
     return tokens;
 }
@@ -88,7 +117,7 @@ function parse(a) {
                 break;
             case("/"):
                 t = stack.pop();
-                stack.push(devide(stack.pop(), t));
+                stack.push(divide(stack.pop(), t));
                 break;
             case("+"):
                 stack.push(add(stack.pop(), stack.pop()));
@@ -97,8 +126,22 @@ function parse(a) {
                 t = stack.pop();
                 stack.push(subtract(stack.pop(), t));
                 break;
+            case("sin"):
+                stack.push(sin(stack.pop()));
+                break;
+            case("cos"):
+                stack.push(cos(stack.pop()));
+                break;
+            case("cuberoot"):
+                stack.push(cuberoot(stack.pop()));
+                break;
+            case("cube"):
+                stack.push(cube(stack.pop()));
+                break;
             default:
-                if (!isNaN(token[i])) {
+                if (token[i] in constNames) {
+                    stack.push(cnst(token[i]));
+                } else if (!isNaN(token[i])) {
                     stack.push(cnst(parseInt(token[i])));
                 } else {
                     stack.push(variable(token[i]));
@@ -107,8 +150,6 @@ function parse(a) {
     }
     return stack[0];
 }
-let exp = parse(' 10         ');
-console.log(exp(0.0));
 
-
-
+let exp = parse("x cube y * z 0 +");
+console.log(exp(3, 10, 3));
