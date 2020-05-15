21a2a99630f181a76b5eb390ab5c0a3e1c7173c4
ricnorr <31213770+ricnorr@users.noreply.github.com>
2020-05-16 00:56:45 +0300

    task12 const works normally with minus in beginning. Need to make code easier

---
+++ task12/src/task12clojure/main.clj
@@ -0,0 +1,241 @@
+(defn -return [value tail] {:value value :tail tail})
+(def -valid? boolean)
+(def -value :value)
+(def -tail :tail)
+
+(defn _show [result]
+  (if (-valid? result) (str "-> " (pr-str (-value result)) " | " (pr-str (apply str (-tail result))))
+                       "!"))
+(defn tabulate [parser inputs]
+  (run! (fn [input] (printf "    %-10s %s\n" input (_show (parser input)))) inputs))
+
+(defn _empty [value] (partial -return value))
+
+(defn _char [p]
+  (fn [[c & cs]]
+    (if (and c (p c)) (-return c cs))))
+
+(defn _map [f result]
+  (if (-valid? result)
+    (-return (f (-value result)) (-tail result))))
+
+(defn _combine [f a b]
+  (fn [str]
+    (let [ar ((force a) str)]
+      (if (-valid? ar)
+        (_map (partial f (-value ar))
+              ((force b) (-tail ar)))))))
+
+(defn _either [a b]
+  (fn [str]
+    (let [ar ((force a) str)]
+      (if (-valid? ar) ar ((force b) str)))))
+
+(defn _parser [p]
+  (fn [input]
+    (-value ((_combine (fn [v _] v) p (_char #{\u0000})) (str input \u0000)))))
+
+
+
+
+(defn +char [chars] (_char (set chars)))
+(defn +char-not [chars] (_char (comp not (set chars))))
+(defn +map [f parser] (comp (partial _map f) parser))
+
+(def +parser _parser)
+
+(def +ignore (partial +map (constantly 'ignore)))
+(defn iconj [coll value]
+  (if (= value 'ignore) coll (conj coll value)))
+(defn +seq [& ps]
+  (reduce (partial _combine iconj) (_empty []) ps))
+(defn +seqf [f & ps] (+map (partial apply f) (apply +seq ps)))
+(defn +seqn [n & ps] (apply +seqf (fn [& vs] (nth vs n)) ps))
+
+(defn *word [word] (apply +seq (reduce #(conj %1 (+char (str %2))) [] word)))
+
+
+
+(defn +or [p & ps]
+  (reduce _either p ps))
+(defn +opt [p]
+  (+or p (_empty nil)))
+
+(defn +star [p]
+  (letfn [(rec [] (+or (+seqf cons p (delay (rec))) (_empty ())))] (rec)))
+(defn +plus [p] (+seqf cons p (+star p)))
+
+
+;;;;
+
+
+
+;;;
+(defn +str [p] (+map (partial apply str) p))
+
+(def *negate (+str (*word "negate")))
+
+(defn proto-get [obj key]
+  (cond
+    (contains? obj key) (obj key)
+    (contains? obj :proto) (proto-get (obj :proto) key)))
+
+(defn proto-call [obj key]
+  (proto-get obj key))
+
+(def opProto)
+
+(defn constructor [proto func differ token]
+  (fn [& vals] {:proto proto :vals vals :func func :differ differ :token token}))
+
+(defn method [key] (fn [obj & args] (apply (proto-call obj key) obj args)))
+
+(def evaluate (method :evaluate))
+(def diff (method :diff))
+(def toStringSuffix (method :toString))
+(def getVals #(proto-get %1 :vals))
+(def getToken #(proto-get %1 :token))
+
+
+
+(def opProto
+  {:evaluate
+   (fn [obj args] (apply (get obj :func) (mapv #(evaluate %1 args) (get obj :vals))))
+   :diff
+   (fn [obj args] (let [vals (getVals obj)
+                        diff_vals (mapv #(diff %1 args) vals)]
+                    (apply (get obj :differ) (concat vals diff_vals))
+                    ))
+   :toString
+   (fn [obj] (str "(" (clojure.string/join " " (mapv toStringSuffix (getVals obj))) " " (getToken obj) ")"))
+   })
+
+
+
+(def Constant)
+
+(def constProto
+  {:evaluate
+   (fn [obj & args] (first (getVals obj)))
+   :toString
+   (fn [obj] (format "%.1f" (double (first (getVals obj)))))
+   :diff
+   (fn [& args] (Constant 0))
+   })
+
+(def variableProto
+  {:evaluate
+   (fn [obj args] (get args (str (first (getVals obj))) nil))
+   :diff
+   (fn [obj args] (if (= args (first (getVals obj))) (Constant 1) (Constant 0)))
+   :toString
+   (fn [obj] (str (first (getVals obj))))
+   })
+
+(def Add (constructor opProto + #(Add %3 %4) "+"))
+(def Subtract (constructor opProto - #(Subtract %3 %4) "-"))
+(def Multiply (constructor opProto * #(Add (Multiply %3 %2) (Multiply %4 %1)) "*"))
+(def Divide (constructor opProto #(/ (double %1) (double %2)) #(Divide (Subtract (Multiply %3 %2) (Multiply %1 %4)) (Multiply %2 %2)) "/"))
+(def Negate (constructor opProto #(- %1) #(Negate %2) "negate"))
+(def Constant (constructor constProto nil nil nil))
+(def Variable (constructor variableProto nil nil nil))
+
+
+(def strOper {"+" Add "-" Subtract "*" Multiply "/" Divide "negate" Negate})
+
+
+
+
+
+(defn extr-const [arg] (Constant (read-string arg)))
+
+
+(defn +str [p] (+map (partial apply str) p))
+(defn +var [p] (+map Variable (+str p)))
+
+
+(def *digit (+char "0123456789"))
+(def *all-chars (mapv char (range 32 128)))
+
+(apply str *all-chars)
+(def *letter (+char (apply str (filter #(Character/isLetter %) *all-chars))))
+
+
+
+;(def *number (+map extr-const (+str (+or (+seq (+char "-") (+plus *digit)) (+plus *digit)))))
+
+(def *number (+map (fn [[a b c d]]  (Constant (read-string (str (str a) (apply str b) (str c) (apply str d))) )) (+seq (+opt (+char "-")) (+plus *digit)    (+opt (+char "."))  (+opt (+plus *digit) )) ))
+
+(def *constant *number)                                     ;ok
+
+(def *var (+var (+star *letter)))                           ;ok
+
+(def *operand (+or *constant *var))
+
+(def *operation (+char "+-*/"))
+
+(def *ob (+ignore (+char "(")))
+
+(def *space (+char " \t\n\r"))
+
+(def *ws (+ignore (+star *space)))
+(def *cb (+ignore (+char ")")))
+
+(def *br_exp)
+(def *full_exp)
+
+
+
+(defn +convert [p]
+  (+map #(let [op (symbol (str (peek %1)))] (apply (get strOper op) (pop %1))) p))
+
+
+(comment  (def bracket_exp (+or (+seqn 1 (+char "(") *ws (delay bracket_exp) *ws (+char ")")) (delay simple) *operand
+                                ((+map
+                                   (fn [[a b c]] ( (get strOper (str c)) a b))
+                                   (+seq
+                                     *ws
+                                     (delay bracket_exp)
+                                     *ws (delay bracket_exp) *ws
+                                     *operation))))) )
+
+
+(def bracket-exp1 (+or (+seq *ws (+char "(") *ws (delay bracket-exp1) *ws (delay bracket-exp1) *ws *operation *ws (+char ")")
+                             )
+                       *operand))
+
+;Testing: (parseObjectSuffix " 10.0         ")
+(comment (def bracket-exp2
+           (+or
+             (+map
+               (fn [[a b c d e]] ((get strOper (str d)) b c))
+               (+seq
+                 *ws (+char "(") *ws
+                 (delay bracket-exp2) *ws (delay bracket-exp2)
+                 *ws *operation *ws (+char ")") *ws))
+             *operand)))
+(declare bracket-exp2)
+
+(def *unary (+map (fn [[a b c d]] ( (println ((get strOper c) b))  (get strOper c) b))
+                  (+seq *ws (+char "(")
+                        *ws bracket-exp2 *ws *negate *ws (+char ")"))))
+
+(def bracket-exp2
+  (+or
+    (+map (fn [[a b c d]] ((get strOper c) b))
+          (+seq *ws (+char "(")
+                *ws (delay bracket-exp2) *ws *negate *ws (+char ")")))
+    (+map
+      (fn [[a b c d e]] ((get strOper (str d)) b c))
+      (+seq
+        *ws (+char "(") *ws
+        (delay bracket-exp2) *ws (delay bracket-exp2)
+        *ws *operation *ws (+char ")") *ws))                ;
+    (+map (fn [[a]] a) (+seq *ws *operand *ws))
+    *operand
+    )
+  )
+
+(defn parseObjectSuffix [str] (get (bracket-exp2 str) :value))
+
+(print (parseObjectSuffix "10"))
---
+++ task12/test/task12clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns task12clojure.core-test
+  (:require [clojure.test :refer :all]
+            [task12clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
