0a7d79db3e5dc904a15de9b284cb2b340ac1be69
ricnorr <31213770+ricnorr@users.noreply.github.com>
2020-05-26 15:09:00 +0300

    mend

--- task12/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to task12clojure
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- task12/test/task12clojure/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns task12clojure.core-test
-  (:require [clojure.test :refer :all]
-            [task12clojure.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
